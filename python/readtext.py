de32668774fca4961132d980ba1cd73dd7879cb9
Sreelakshmi KebbahallyChikkalingegowda <Sreelakshmi.KebbahallyChikkalingegowda@elektrobit.com>
2020-07-24 02:17:20 +0200

    merged from master

--- python/readtext.py
+++ python/readtext.py
@@ -2,9 +2,7 @@
 # -*- coding: utf-8 -*-
 
 print("here i am reading the text file")
-<<<<<<< HEAD
-print (" development changes")
-print("development 1")
-=======
+#print (" development changes")
+#print("development 1")
 file =open('logcat.txt', 'w')
->>>>>>> master
+
