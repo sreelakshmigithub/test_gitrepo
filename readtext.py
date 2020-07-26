30b78073dd0f17103d8bdde56f9bdae666d3cd17
Sreelakshmi KebbahallyChikkalingegowda <Sreelakshmi.KebbahallyChikkalingegowda@elektrobit.com>
2020-07-26 22:37:47 +0200

    duplicate file removed

--- readtext.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-print("here i am reading the text file")
-file =open('logcat.txt', 'w')
-# write() - it used to write direct text to the file
-# writelines() - it used to write multiple lines or strings at a time, it takes iterator as an argument
-# writing data using the write() method
-
