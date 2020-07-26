45ab435a08f794ab7972f6819896ae983e891029
Sreelakshmi KebbahallyChikkalingegowda <Sreelakshmi.KebbahallyChikkalingegowda@elektrobit.com>
2020-07-26 22:57:17 +0200

    master branch changes to repo

--- python/readtext.py
+++ python/readtext.py
@@ -5,4 +5,5 @@ print("here i am reading the text file")
 #print (" development changes")
 #print("development 1")
 file =open('logcat.txt', 'w')
-
+file = write("writing to file \n")
+print("This line is from master branch")
